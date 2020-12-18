46db5ba602c116e8de5b832af7dae5fa26f63061
Patryk Kiepas <kiepas.patryk@gmail.com>
2020-12-18 01:06:07 +0100

    In memory measurement (#3)
    * Update peptalk-core
    
    * Integrating the new interface for the in-file counting/profiling
    
    * Correcting the in-code documentation
    
    * Adding full-implementation of the in memory profiling

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 
 #include "timing.hpp"
 #include "counting.hpp"
+#include "counting_in_file.hpp"
 #include "profiling.hpp"
+#include "profiling_in_file.hpp"
 
 using namespace peptalk;
+using std::map;
+using std::pair;
 using std::runtime_error;
 using std::string;
 using std::vector;
@@ -37,36 +41,84 @@ void TimingClose() {
 }
 
 /*
- * Counting
+ * Counting in file
  */
-void CountingInit(const string &result_file,
+void CountingInFileInit(const string &result_file,
                   const vector<string> &parameters,
                   const vector<string> &performance_events,
                   unsigned int num_measurements) {
-    counting::Init(result_file, parameters, performance_events, num_measurements, OnErrorOrWarning);
+    counting_in_file::Init(result_file, parameters, performance_events, num_measurements, OnErrorOrWarning);
 }
 
-void CountingStart(const vector<string> &parameters) {
-    counting::Start(parameters, OnErrorOrWarning);
+void CountingInFileStart(const vector<string> &parameters) {
+    counting_in_file::Start(parameters, OnErrorOrWarning);
 }
 
-void CountingStop() {
-    counting::Stop(OnErrorOrWarning);
+void CountingInFileStop() {
+    counting_in_file::Stop(OnErrorOrWarning);
 }
 
-void CountingClose() {
-    counting::Close(OnErrorOrWarning);
+void CountingInFileClose() {
+    counting_in_file::Close(OnErrorOrWarning);
 }
 
 /*
- * Profiling
+ * Profiling in memory
  */
-void ProfilingInit(const string &result_file,
+void ProfilingInit(const vector<string> &events,
+                   bool instruction_address,
+                   int overflow_threshold) {
+    profiling::Init(events, instruction_address, overflow_threshold, OnErrorOrWarning);
+}
+
+void ProfilingStart() {
+    profiling::Start(OnErrorOrWarning);
+}
+
+void ProfilingStop() {
+    profiling::Stop(OnErrorOrWarning);
+}
+
+void ProfilingCleanup() {
+    profiling::Cleanup(OnErrorOrWarning);
+}
+
+int ProfilingNumProfile() {
+    return profiling::GetNumProfile();
+}
+
+const vector<peptalk::measurement_type>& ProfilingGetProfile(int index) {
+    return profiling::GetProfile(index);
+}
+
+const vector<peptalk::inst_address_type>& ProfilingGetInstructionAddress() {
+    return profiling::GetInstructionAddress();
+}
+
+string ProfilingGetEvent(int index) {
+    return profiling::GetProfileEvent(index);
+}
+
+vector<pair<string, vector<peptalk::measurement_type>>> ProfilingGetMeasurements() {
+    vector<pair<string, vector<peptalk::measurement_type>>> dict;
+    for (int profile_idx = 0; profile_idx < profiling::GetNumProfile(); ++profile_idx) {
+        auto event = profiling::GetProfileEvent(profile_idx);
+        auto measurements = profiling::GetProfile(profile_idx);
+        dict.push_back({event, {}});
+        dict[profile_idx].second.insert(dict[profile_idx].second.end(), measurements.begin(), measurements.end());
+    }
+    return dict;
+}
+
+/*
+ * Profiling in file
+ */
+void ProfilingInFileInit(const string &result_file,
                    const string &overflow_event,
                    int overflow_threshold,
                    const vector<string> &performance_events,
                    bool include_inst_address) {
-    profiling::Init(
+    profiling_in_file::Init(
             result_file,
             overflow_event,
             overflow_threshold,
@@ -75,16 +127,16 @@ void ProfilingInit(const string &result_file,
             OnErrorOrWarning);
 }
 
-void ProfilingStart(const string &trace_header) {
-    profiling::Start(trace_header, OnErrorOrWarning);
+void ProfilingInFileStart(const string &trace_header) {
+    profiling_in_file::Start(trace_header, OnErrorOrWarning);
 }
 
-void ProfilingStop() {
-    profiling::Stop(OnErrorOrWarning);
+void ProfilingInFileStop() {
+    profiling_in_file::Stop(OnErrorOrWarning);
 }
 
-void ProfilingClose() {
-    profiling::Close(OnErrorOrWarning);
+void ProfilingInFileClose() {
+    profiling_in_file::Close(OnErrorOrWarning);
 }
 
 namespace py = pybind11;
@@ -100,14 +152,23 @@ PYBIND11_MODULE(peptalk, m) {
             timing_start
             timing_stop
             timing_close
-            counting_init
-            counting_start
-            counting_stop
-            counting_close
+            counting_in_file_init
+            counting_in_file_start
+            counting_in_file_stop
+            counting_in_file_close
             profiling_init
             profiling_start
             profiling_stop
-            profiling_close
+            profiling_cleanup
+            profiling_num_profile
+            profiling_get_profile
+            profiling_get_inst_address
+            profiling_get_event
+            profiling_get_measurements
+            profiling_in_file_init
+            profiling_in_file_start
+            profiling_in_file_stop
+            profiling_in_file_close
     )pbdoc";
     /*
      * Timing
@@ -125,34 +186,64 @@ PYBIND11_MODULE(peptalk, m) {
         Close timing module
     )pbdoc");
     /*
-     * Counting
+     * Counting in file
      */
-    m.def("counting_init", &CountingInit, R"pbdoc(
-        Initialise counting module
+    m.def("counting_in_file_init", &CountingInFileInit, R"pbdoc(
+        Initialise in-file counting module
     )pbdoc");
-    m.def("counting_start", &CountingStart, R"pbdoc(
-        Start counting
+    m.def("counting_in_file_start", &CountingInFileStart, R"pbdoc(
+        Start in-file counting
     )pbdoc");
-    m.def("counting_stop", &CountingStop, R"pbdoc(
-        Stop counting
+    m.def("counting_in_file_stop", &CountingInFileStop, R"pbdoc(
+        Stop in-file counting
     )pbdoc");
-    m.def("counting_close", &CountingClose, R"pbdoc(
-        Close counting module
+    m.def("counting_in_file_close", &CountingInFileClose, R"pbdoc(
+        Close in-file counting module
     )pbdoc");
     /*
-     * Profiling
+     * Profiling in memory
      */
     m.def("profiling_init", &ProfilingInit, R"pbdoc(
-        Initialise profiling module
+        Initialise in-memory profiling module
     )pbdoc");
     m.def("profiling_start", &ProfilingStart, R"pbdoc(
-        Start profiling
+        Start in-memory profiling
     )pbdoc");
     m.def("profiling_stop", &ProfilingStop, R"pbdoc(
-        Stop profiling
+        Stop in-memory profiling
     )pbdoc");
-    m.def("profiling_close", &ProfilingClose, R"pbdoc(
-        Close profiling module
+    m.def("profiling_cleanup", &ProfilingCleanup, R"pbdoc(
+        Cleanup in-memory profiling module
+    )pbdoc");
+    m.def("profiling_num_profile", &ProfilingNumProfile, R"pbdoc(
+        Return number of measured profiles
+    )pbdoc");
+    m.def("profiling_get_profile", &ProfilingGetProfile, R"pbdoc(
+        Get profile measurements
+    )pbdoc", py::arg("index"));
+    m.def("profiling_get_inst_address", &ProfilingGetInstructionAddress, R"pbdoc(
+        Get address of profiled instructions
+    )pbdoc");
+    m.def("profiling_get_event", &ProfilingGetEvent, R"pbdoc(
+        Get profile event
+    )pbdoc", py::arg("index"));
+    m.def("profiling_get_measurements", &ProfilingGetMeasurements, R"pbdoc(
+        Return measurements for profiles
+    )pbdoc");
+    /*
+     * Profiling in file
+     */
+    m.def("profiling_in_file_init", &ProfilingInFileInit, R"pbdoc(
+        Initialise in-file profiling module
+    )pbdoc");
+    m.def("profiling_in_file_start", &ProfilingInFileStart, R"pbdoc(
+        Start in-file profiling
+    )pbdoc");
+    m.def("profiling_in_file_stop", &ProfilingInFileStop, R"pbdoc(
+        Stop in-file profiling
+    )pbdoc");
+    m.def("profiling_in_file_close", &ProfilingInFileClose, R"pbdoc(
+        Close in-file profiling module
     )pbdoc");
     m.attr("__version__") = "1.0";
 }
--- test.py
+++ test.py
@@ -5,12 +5,21 @@ peptalk.timing_start(["no_instructions"])
 peptalk.timing_stop()
 peptalk.timing_close()
 
-peptalk.counting_init("performance_counters.csv", ["test"], ["INST_RETIRED:ANY_P", "CPU_CLK_UNHALTED:THREAD_P"], 1)
-peptalk.counting_start(["no_instructions"])
-peptalk.counting_stop()
-peptalk.counting_close()
+peptalk.counting_in_file_init("performance_counters.csv", ["test"], ["INST_RETIRED:ANY_P", "CPU_CLK_UNHALTED:THREAD_P"], 1)
+peptalk.counting_in_file_start(["no_instructions"])
+peptalk.counting_in_file_stop()
+peptalk.counting_in_file_close()
 
-peptalk.profiling_init("performance_profiles.pep", "INST_RETIRED:ANY_P", 2000, ["CPU_CLK_UNHALTED:THREAD_P"], True)
-peptalk.profiling_start("no_instructions")
+peptalk.profiling_in_file_init("performance_profiles.pep", "INST_RETIRED:ANY_P", 2000, ["CPU_CLK_UNHALTED:THREAD_P"], True)
+peptalk.profiling_in_file_start("no_instructions")
+peptalk.profiling_in_file_stop()
+peptalk.profiling_in_file_close()
+
+peptalk.profiling_init(["INST_RETIRED:ANY_P", "CPU_CLK_UNHALTED:THREAD_P", "L1D:REPLACEMENT"], True, 1000)
+peptalk.profiling_start()
 peptalk.profiling_stop()
-peptalk.profiling_close()
+print("Profiles={}".format(peptalk.profiling_get_measurements()))
+print("Inst address={}".format(peptalk.profiling_get_inst_address()))
+peptalk.profiling_cleanup()
+
+
