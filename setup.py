1e55b7e5453277e9fbc08bfb16b3da12f31a4c4e
Patryk Kiepas <kiepas.patryk@gmail.com>
2020-12-21 23:23:18 +0100

    Update peptalk core (#5)
    * Update peptalk-core
    
    * Mixing python pure scripts with generated C bindings (pybind11) in one package. Easy, high-level module for profiling (start/stop&print)
    
    * Move pybind11 bindings to src/ directory
    
    * Removing redundant function redefintions
    
    * Bump package version to 0.1.0
    
    * Simplifying the setup script for the cmake build
    
    * Simplify the setup/build scripts; adding some comments

--- setup.py
+++ setup.py
@@ -1,43 +1,38 @@
 import os
-import re
 import sys
 import platform
 import subprocess
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
 
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
+        # Reconstruct an absolute path to our cmake module
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
     def run(self):
+        # Check if cmake is installed
         try:
             out = subprocess.check_output(['cmake', '--version'])
         except OSError:
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
+        # Build each extension from the `ext_modules` field (including our CMakeExtension)
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
+        # Path to the compiled module (e.g. peptalk_core_impl.cpython3.8*.so)
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        # required for auto-detection of auxiliary "native" libs
+        # Check if path ends with the separator
+        # Required for auto-detection of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
-
-        print('extdir' + extdir)
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
@@ -57,21 +52,24 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
+        # Create build directory
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+        # Generate build files for the cmake module
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        # Build the cmake module
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
     name="peptalk",
-    version="0.0.1",
+    version="0.1.0",
     author="quepas",
     author_email="kiepas.patryk@gmail.com",
     description="",
     long_description='',
     url="https://github.com/quepas/peptalk-py.git",
     license="MIT License",
-    ext_modules=[CMakeExtension('peptalk')],
+    ext_modules=[CMakeExtension('peptalk_core_impl')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
 )
